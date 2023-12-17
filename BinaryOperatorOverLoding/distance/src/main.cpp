d0340b73a2e5f56827e8f474ef55f2e8dc08e436
Dineshs737 <dinesh77saarck@gmail.com>
2023-12-17 03:54:40 +0200

    update

--- BinaryOperatorOverLoding/distance/src/main.cpp
+++ BinaryOperatorOverLoding/distance/src/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main()
 {
-    Distance d1;
+    Distance d1,d2,d3;
     cin>> d1;
-    cout<<d1;
+    cin >>d2;
+   // cout<<d1;
+
+   d3 =d1+d2;
+   cout<<d3;
+
+
     
 }
